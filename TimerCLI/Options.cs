bc0124a7eb74ca3ed277395fa7530296fcf9665c
Tomas Bouda <email@tomasbouda.cz>
2024-05-29 09:53:48 +0200

    Initial commit

---
+++ TimerCLI/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+public class Options
+{
+    [Option('h', "hours", Required = false, HelpText = "Hours to wait")]
+    public int Hours { get; set; }
+    
+    [Option('m', "minutes", Required = false, HelpText = "Minutes to wait")]
+    public int Minutes { get; set; }
+    
+    [Option('s', "seconds", Required = false, HelpText = "Seconds to wait")]
+    public int Seconds { get; set; }
+}
